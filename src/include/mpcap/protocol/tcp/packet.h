7046664a8b115fdb2e19b39b6dc6d3fb7ad7d08b
soba1104 <shiba@rvm.jp>
2014-05-11 21:18:34 +0900

    define tcp header struct

--- src/include/mpcap/protocol/tcp/packet.h
+++ src/include/mpcap/protocol/tcp/packet.h
@@ -6,7 +6,6 @@
 #include <mpcap/protocol/tcp.h>
 #include <mpcap/protocol/tcp/address.h>
 
-#include <netinet/tcp.h>
 #include <string>
 
 namespace mpcap {
@@ -14,29 +13,42 @@ namespace mpcap {
 namespace protocol {
 
 class tcp::packet : public iface::packet {
+  struct header {
+    uint16_t srcport;
+    uint16_t dstport;
+    uint32_t seqnum;
+    uint32_t acknum;
+    uint8_t offset;
+    uint8_t flags;
+    uint16_t window;
+    uint16_t checksum;
+    uint16_t urgptr;
+  };
+
   public:
     packet(void) {}
     ~packet(void) {}
 
     template<typename NEXT> bool apply(const void *data, int32_t size);
     inline virtual int32_t size(void) const final override { return m_size; }
-    inline virtual const void *ptr(void) const final override { return m_thdr; }
+    inline virtual const void *ptr(void) const final override { return m_header; }
     inline virtual int32_t datasize(void) const final override { return m_datasize; }
     inline virtual const void *dataptr(void) const final override { return m_dataptr; }
 
-    inline uint16_t srcport(void) const { return m_thdr->source; }
-    inline uint16_t dstport(void) const { return m_thdr->dest; }
-    inline uint32_t seqnum(void) const { return m_thdr->seq; }
-    inline uint32_t acknum(void) const { return m_thdr->ack_seq; }
-    inline bool fin(void) const { return m_thdr->fin == 1; }
-    inline bool syn(void) const { return m_thdr->syn == 1; }
-    inline bool rst(void) const { return m_thdr->rst == 1; }
-    inline bool psh(void) const { return m_thdr->psh == 1; }
-    inline bool ack(void) const { return m_thdr->ack == 1; }
-    inline bool urg(void) const { return m_thdr->urg == 1; }
-    inline uint16_t window(void) const { return m_thdr->window; }
-    inline uint16_t checksum(void) const { return m_thdr->check; }
-    inline uint16_t urgptr(void) const { return m_thdr->urg_ptr; }
+    inline uint16_t srcport(void) const { return m_header->srcport; }
+    inline uint16_t dstport(void) const { return m_header->dstport; }
+    inline uint32_t seqnum(void) const { return m_header->seqnum; }
+    inline uint32_t acknum(void) const { return m_header->acknum; }
+    inline uint8_t offset(void) const { return (m_header->offset & 0xf0) >> 4; }
+    inline bool fin(void) const { return m_header->flags & 0x01; }
+    inline bool syn(void) const { return m_header->flags & 0x02; }
+    inline bool rst(void) const { return m_header->flags & 0x04; }
+    inline bool psh(void) const { return m_header->flags & 0x08; }
+    inline bool ack(void) const { return m_header->flags & 0x10; }
+    inline bool urg(void) const { return m_header->flags & 0x20; }
+    inline uint16_t window(void) const { return m_header->window; }
+    inline uint16_t checksum(void) const { return m_header->checksum; }
+    inline uint16_t urgptr(void) const { return m_header->urgptr; }
 
     inline address src(void) const { return address(srcport()); }
     inline address dst(void) const { return address(dstport()); }
@@ -46,10 +58,10 @@ class tcp::packet : public iface::packet {
     }
 
     inline virtual bool apply(const void *data, int32_t size) final override {
-      m_thdr = static_cast<const struct tcphdr*>(data);
+      m_header = static_cast<const struct header*>(data);
       m_size = size;
       const uint8_t *__data = static_cast<const uint8_t*>(data);
-      uint16_t hdrsize = m_thdr->doff * sizeof(uint32_t);
+      uint16_t hdrsize = offset() * sizeof(uint32_t);
       m_dataptr = static_cast<const void*>(__data + hdrsize);
       m_datasize = m_size - hdrsize;
       return m_datasize >= 0;
@@ -58,7 +70,7 @@ class tcp::packet : public iface::packet {
     std::string inspect(void) const;
 
   private:
-    const struct tcphdr *m_thdr;
+    const struct header *m_header;
     const void *m_dataptr;
     int32_t m_size;
     int32_t m_datasize;
